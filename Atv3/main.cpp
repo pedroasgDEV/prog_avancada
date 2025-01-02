e290cb906054e1b6b76161ce6adf6161f265ba90
Pedro Augusto <pedro.asg@aluno.ufop.edu.br>
2025-01-02 10:14:59 -0300

    test atv3

--- Atv3/main.cpp
+++ Atv3/main.cpp
@@ -1,70 +1,91 @@
 #include <iostream>
+#include <ostream>
 #include <vector>
+#include <set>
 #include <string>
+#include <cmath>
 
 
 using namespace  std;
 
-string solution(vector<string> in) {
+set<string> solution(vector<string> in, string max, string min) {
     int size_in = in.size(); // Tamanho do vetor de entrada
-    string result = ""; // Resultado final
-    string smaller = in[0]; // Menor arquivo
-    string bigger; // Maior arquivo oposto ao menor arquivo
-    int size_smaller; // Tamanho da menor palavra
-    int smaller_position = 0; // Qual a posição da menor palavra se é no inicio (-1) ou no final (1) ou indefinido (0)
-    vector<string> aux; // Vetor auxiliar para armazenar os strings opostas a menor
+    set<string> result; // Soluções possiveis
+    int size_smaller; // Tamanho do menor fragmento
+    int smaller_position = 0; // Qual a posição do menor fragmento se é no inicio (-1) ou no final (1) ou indefinido (0)
+    vector<string> aux; // Vetor auxiliar para armazenar os fragmento opostos a menor fragmento
     int size_aux; // Tamanho do vetor auxiliar
-    string init, end; // O inicio e o final de uma string
+    string init, end; // O inicio e o final de um fragmento
 
 
-    // Se só tiverem dois arquivos quebrados
-    if ( size_in == 2 ) return in[0] + in[1] + '\n' + in[1] + in[0] + '\n';
+    // Se não for possivel encontrar somente uma solução valida
+    if ( size_in % 2 != 0 || size_in == 2) { // Se a quantidae de fragmentos for impar ou igual a 2
+        // Imprime todas as soluções possiveis
+        for (auto i : in)
+            for (auto j : in) {
+                //Se a solução for maior que uma solução possivel
+                if( i == j || (i + j).length() > (min + max).length() ) continue;
+                result.insert(i + j);
+                result.insert(j + i);
+            }
+        return  result;
+    }
 
-    //Encontra o menor arquivo
-    for(int i = 1; i < size_in; i++)
-        if ( in[i].length() < smaller.length() ) smaller = in[i];
-
-    size_smaller = smaller.length();
+    size_smaller = min.length();
 
     
     for(auto i : in){
-        if(i == smaller) continue; // Pula a menor palavra achada anteriormente
+        if(i == min) continue; // Pula o menor fragmento achado anteriormente
         
-        // Se a posição da menor palavra for no incio ou indefinida
+        // Se a posição do menor fragmento for no incio ou indefinida
         if (smaller_position <= 0 ) {
-            init = i.substr(0, size_smaller); // Pega o inicio correspondente ao tamanho do menor arquivo
+            init = i.substr(0, size_smaller); // Pega o inicio correspondente ao tamanho do menor fragmento
             
-            if(init == smaller){ // Se o inicio do arquivo for igual ao menor arquivo
-                smaller_position = -1; // Define a posição da menor palavra
+            if(init == min){ // Se o inicio do fragmento for igual ao menor fragmento
+                smaller_position = -1; // Define a posição do menor fragmento
                 continue;
             }
         }
 
-        // Se a posição da menor palavra for no final ou indefinida
+        // Se a posição do menor fragmento for no final ou indefinida
         if (smaller_position >= 0) {
-            end = i.substr(i.length() - size_smaller, size_smaller); // Pega o final correspondente ao tamanho do menor arquivo
+            end = i.substr(i.length() - size_smaller, size_smaller); // Pega o final correspondente ao tamanho do menor fragmento
 
-            if(end == smaller) { // Se o final do arquivo for igual ao menor arquivo
-                smaller_position = 1; // Define a posição da menor palavra
+            if(end == min) { // Se o final do fragmento for igual ao menor fragmento
+                smaller_position = 1; // Define a posição do menor fragmento
                 continue;
             }
         }
 
-        // Se o arquivo for na posição oposta do menor arquivo adiciona ao vetor auxiliar
+        // Se o fragmento for na posição oposta do menor fragmento adiciona ao vetor auxiliar
         aux.push_back(i);
 
     }
 
     size_aux = aux.size(); // Pega o tamanho do vetor auxiliar
-    bigger = aux[0];
 
-    // Encontra o maior arquivo oposto ao menor arquivo
+    // Se há fragmentos opostos a mais ou a menos
+    if ( size_aux != size_in/2 - 1){
+        // Imprime todas as soluções possiveis
+        for (auto i : in)
+            for (auto j : in) {
+                //Se a solução for maior que uma solução possivel
+                if( i == j || (i + j).length() > (min + max).length() ) continue;
+                result.insert(i + j);
+                result.insert(j + i);
+            }
+        return  result;
+    }
+
+    string bigger = aux[0]; // Maior do vetor auxiliar 
+
+    // Encontra o maior fragmento oposto ao menor fragmento
     for(int i = 1; i < size_aux; i++)
         if ( aux[i].length() > bigger.length() ) bigger = aux[i];
 
-    // Concatena os dois arquivos com base na posição do menor
-    if (smaller_position == -1) result = smaller + bigger + '\n';
-    if (smaller_position == 1) result = bigger + smaller + '\n';
+    // Concatena os dois fragmentos com base na posição do menor
+    if (smaller_position == -1) result.insert(min + bigger);
+    if (smaller_position == 1) result.insert(bigger + min);
 
     return  result;
 }
@@ -73,6 +94,12 @@ string solution(vector<string> in) {
 int main() {
     int N; // Quantidade de casos de teste
     string line;
+    string min; // Menor fragmento 
+    string max; // maior fragmento
+    int min_size = 256; // Tamanho do menor fragmento
+    int max_size = 0; // Tamanho do maior fragmento
+
+
     getline(cin, line); // Le a quantidade de casos de teste
     N = stoi(line);
     getline(cin, line); // Le a linha em branco
@@ -83,13 +110,18 @@ int main() {
 
         // Armazenas os arquvios quebrados
         while (getline(cin, line) && !line.empty()) {
+            if( (int) line.length() < min_size) { min = line; min_size = min.length(); }
+            if( (int) line.length() > max_size) { max = line; max_size = max.length(); }
             files.push_back(line);
         }
 
         // Se não tiverem aquivos quebrados tenta esse caso de teste novamente
         if(files.empty()) { i--; continue; }
 
-        cout << solution(files) << endl;
+        set<string> result = solution(files, max, min);
+
+        for( auto i : result ) cout << i << endl;
+        cout << endl;
     }
 
     return 0;
