d0f37fd002e10826fabd5c5757c87509f5095b2a
Pedro Augusto <pedro.asg@aluno.ufop.edu.br>
2025-01-02 21:59:19 -0300

    almost finish atv3

--- Atv3/main.cpp
+++ Atv3/main.cpp
@@ -8,12 +8,13 @@
 
 using namespace  std;
 
-set<string> solution(vector<string> in, string max, string min) {
+set<string> solution(vector<string> in, string min, string max) {
     int size_in = in.size(); // Tamanho do vetor de entrada
     set<string> result; // Soluções possiveis
     int size_smaller = min.length(); // Tamanho do menor fragmento
     int smaller_position = 0; // Qual a posição do menor fragmento se é no inicio (-1) ou no final (1) ou indefinido (0)
-    vector<string> aux; // Vetor auxiliar para armazenar os fragmento opostos a menor fragmento
+    vector<string> aux; // Vetor auxiliar para armazenar os fragmento opostos a menor fragmento;
+    string bigger; // Maior fragmento oposto ao menor fragmento
     int size_aux; // Tamanho do vetor auxiliar
     string init, end; // O inicio e o final de um fragmento
 
@@ -24,7 +25,7 @@ set<string> solution(vector<string> in, string max, string min) {
         for (auto i : in)
             for (auto j : in) {
                 //Se a solução for maior que uma solução possivel
-                if( i == j || (i + j).length() > (min + max).length() ) continue;
+                if( i == j ) continue;
                 result.insert(i + j);
             }
         return  result;
@@ -39,7 +40,7 @@ set<string> solution(vector<string> in, string max, string min) {
             init = i.substr(0, size_smaller); // Pega o inicio correspondente ao tamanho do menor fragmento
             
             if(init == min){ // Se o inicio do fragmento for igual ao menor fragmento
-                smaller_position = -1; // Define a posição do menor fragmento
+                smaller_position += -1; // Define a posição do menor fragmento
                 continue;
             }
         }
@@ -49,7 +50,7 @@ set<string> solution(vector<string> in, string max, string min) {
             end = i.substr(i.length() - size_smaller, size_smaller); // Pega o final correspondente ao tamanho do menor fragmento
 
             if(end == min) { // Se o final do fragmento for igual ao menor fragmento
-                smaller_position = 1; // Define a posição do menor fragmento
+                smaller_position += 1; // Define a posição do menor fragmento
                 continue;
             }
         }
@@ -62,21 +63,29 @@ set<string> solution(vector<string> in, string max, string min) {
     size_aux = aux.size(); // Pega o tamanho do vetor auxiliar
 
     // Se não for possivel encontrar somente uma solução valida
-    // Se há fragmentos opostos a mais ou a menos
+    // Se há fragmentos opostos a mais ou a menos que o ideal
     if ( size_aux != size_in/2 - 1){
         // Imprime todas as soluções possiveis
         for (auto i : in)
             for (auto j : in) {
                 //Se a solução for maior que uma solução possivel
-                if( i == j || (i + j).length() > (min + max).length() ) continue;
+                if( i == j ) continue;
                 result.insert(i + j);
             }
         return  result;
     }
 
+    bigger = aux[0];
+
+    // Encontra o maior fragmento oposto
+    for(auto i : aux) 
+        if(i.length() > bigger.length()) bigger = i;
+
+    // Pega a maior string do vetor oposto
+
     // Concatena os dois fragmentos com base na posição do menor
-    if (smaller_position == -1) result.insert(min + max);
-    if (smaller_position == 1) result.insert(max + min);
+    if (smaller_position < 0) result.insert(min + bigger);
+    if (smaller_position > 0) result.insert(bigger + min);
 
     return  result;
 }
@@ -86,10 +95,9 @@ int main() {
     int N; // Quantidade de casos de teste
     string line;
     string min; // Menor fragmento 
-    string max; // maior fragmento
+    string max; // Maior fragmento
     int min_size = 256; // Tamanho do menor fragmento
     int max_size = 0; // Tamanho do maior fragmento
-
 
     getline(cin, line); // Le a quantidade de casos de teste
     N = stoi(line);
@@ -101,6 +109,7 @@ int main() {
 
         // Armazenas os arquvios quebrados
         while (getline(cin, line) && !line.empty()) {
+            // Encontra o menor e o maior fragmento
             if( (int) line.length() < min_size) { min = line; min_size = min.length(); }
             if( (int) line.length() > max_size) { max = line; max_size = max.length(); }
             files.push_back(line);
@@ -109,7 +118,8 @@ int main() {
         // Se não tiverem aquivos quebrados tenta esse caso de teste novamente
         if(files.empty()) { i--; continue; }
 
-        set<string> result = solution(files, max, min);
+        // Busca pelas soluções
+        set<string> result = solution(files, min, max);
 
         for( auto i : result ) cout << i << endl;
         cout << endl;
